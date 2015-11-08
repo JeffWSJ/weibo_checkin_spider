b185560cbd2123ed53b1a403e3596865fc2c4aeb
Jack Criss <1075704670@qq.com>
2015-11-08 21:18:25 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 #coding=utf-8
-import weibo,glob
+import weibo
 import json
 
-APP_KEY = '3445172026'
-APP_SECRET = '577a0175b54ee77ed70d191348f116a6'
+APP_KEY = 'XXXXXXXXXXXXXX'#在新浪申请一个APP
+APP_SECRET = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
 CALL_BACK = 'https://api.weibo.com/oauth2/default.html'
 def run():
         client = weibo.APIClient(APP_KEY, APP_SECRET, CALL_BACK)
@@ -13,34 +13,36 @@ def run():
         r = client.request_access_token(code)
         client.set_access_token(r.access_token, r.expires_in)
         lat_long_file = open("poi.txt","r")
-        i = 0
-        cache = []
-        for i in range (303):
+        cache = []#缓存以确保不重复
+        for lines in lat_long_file:
                 try:
                         checkin_num = 0
                         title = None
-                        lat_long = lat_long_file.readline().strip("\n").split(",")
+                        lat_long = lines.strip("\n").split(",")
                         longitude = lat_long[0]
                         latitude = lat_long[1]
-                        poi_result = str(client.place.nearby.pois.get(lat=latitude,long=longitude))
+                        poi_result = str(client.place.nearby.pois.get(lat=latitude,long=longitude))#返回附近多个poi点的信息
                         poi_list = eval(poi_result)
                         poi_string = json.dumps(poi_list)
                         poi_json = json.loads(poi_string)
-                        if poi_json["pois"][0]["poiid"] in cache:
-                                pass
-                        else:
-                                poi_id = poi_json["pois"][0]["poiid"]
-                                cache.append(poi_id)
-                                checkin_result=str(client.place.pois.show.get(poiid=poi_id))
-                                checkin_list = eval(checkin_result)
-                                checkin_string = json.dumps(checkin_list)
-                                checkin_json = json.loads(checkin_string)
-                                checkin_num = checkin_json["checkin_user_num"]
-                                title = checkin_json["title"]
-                                if checkin_num !=0 and title != None:
-                                        print "%s\t%s\t%s\t%s"%(longitude,latitude,checkin_num,title)
+                        for i in range(0,len(poi_json["pois"])):#把附近所有的poi点都遍历一遍
+                                if poi_json["pois"][i]["poiid"] in cache:
+                                        pass
+                                else:
+                                        poi_id = poi_json["pois"][i]["poiid"]
+                                        cache.append(poi_id)
+                                        checkin_result=str(client.place.pois.show.get(poiid=poi_id))
+                                        checkin_list = eval(checkin_result)
+                                        checkin_string = json.dumps(checkin_list)
+                                        checkin_json = json.loads(checkin_string)
+                                        checkin_num = checkin_json["checkin_num"]#签到数
+                                        lon = checkin_json["lon"]#经度
+                                        lat = checkin_json["lat"]#纬度
+                                        title = checkin_json["title"]#地点名称
+                                        if checkin_num !=0 and title != None:
+                                                print "%s\t%s\t%s\t%s"%(lon,lat,checkin_num,title)
                 except:
-                        pass
+                        print "ERROR!!!!"
 
 if __name__ == "__main__":
 	run()
